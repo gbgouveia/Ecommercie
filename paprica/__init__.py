d5ac4964ed2643bf473755e52aa366d802dcdc43
gbgouveia <gb.gouveia.ps@gmail.com>
2026-02-20 17:34:55 -0300

    Atualizaçao05

--- paprica/__init__.py
+++ paprica/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///paprica.db"
+app.config["SECRET_KEY"] = '8697d8a1ec77e8c9b434ae5a'
 db.init_app(app)
 
 from paprica import routes
